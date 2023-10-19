fa73512d96e2e4edbf2d1cf4d147a485ac456a65
Taher Ahmed <taher1822001@gmail.com>
2023-10-19 21:47:14 +0530

    cc

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,10 @@
 echo "BUILD START"
-wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+curl -L -o Python-3.7.2.tgz https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzf Python-3.7.2.tgz
-cd Python-3.7.2 
-./configure --enable-optimizations 
-make altinstall 
-rm Python-3.7.11.tgz
-# pip install --upgrade sqlite
+cd Python-3.7.2
+./configure --enable-optimizations
+make altinstall
+python3.7 --version
 python3.7 -m pip install -r requirements.txt
 python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
