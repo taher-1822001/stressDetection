2dbab01d44e4689c77b98064cc017ebe38eb8471
Taher Ahmed <taher1822001@gmail.com>
2023-10-19 23:13:57 +0530

    ffgg

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,7 @@ echo "BUILD START"
 curl -L -o Python-3.7.2.tgz https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzf Python-3.7.2.tgz
 cd Python-3.7.2
+ls
 ./configure --enable-optimizations
 make altinstall
 python3.7 --version
