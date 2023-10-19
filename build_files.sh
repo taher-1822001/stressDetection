f2fdb38e9f73d5a1e82af21823c25b80f319a231
Taher Ahmed <taher1822001@gmail.com>
2023-10-19 19:38:29 +0530

    shell update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.7.2 -m pip install -r requirements.txt
-python3.7.2 manage.py collectstatic --noinput --clear
+python3.7 -m pip install -r requirements.txt
+python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
