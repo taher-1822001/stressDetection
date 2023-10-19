7402899d8ecc78c68d60d44ef8f5fd68308f771e
Taher Ahmed <taher1822001@gmail.com>
2023-10-19 21:40:50 +0530

    dfgfhaf

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 echo "BUILD START"
-yum install python3
-pip install --upgrade sqlite
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar xzf Python-3.7.2.tgz
+cd Python-3.7.2 
+./configure --enable-optimizations 
+make altinstall 
+rm Python-3.7.11.tgz
+# pip install --upgrade sqlite
 python3.7 -m pip install -r requirements.txt
 python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
