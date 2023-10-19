e77190f0f3bb24a89cdab353f1167650517d8024
Taher Ahmed <taher1822001@gmail.com>
2023-10-19 21:28:39 +0530

    final changes

--- StressDetection/settings.py
+++ StressDetection/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static'), ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 yum install python3
+pip install --upgrade sqlite
 python3.7 -m pip install -r requirements.txt
 python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
